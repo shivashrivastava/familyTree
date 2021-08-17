495e4a7faf9a0277664caf8fa21f2632b05e1039
nikunj shrivastava <shrivastavanikunj@gmail.com>
2021-08-17 23:01:24 +0530

    comit dropdpwn

--- src/main/webapp/js/VanshavaliAddUpdate.js
+++ src/main/webapp/js/VanshavaliAddUpdate.js
@@ -35,6 +35,15 @@ var Gender = null ;
 
 var resp;
 
+$("#selectmemberdropdown").change(function() {
+    // Pure JS
+    var selectedVal = this.value;
+    var selectedText = this.options[this.selectedIndex].text;
+
+    // jQuery
+    var selectedVal = $(this).find(':selected').val();
+    var selectedText = $(this).find(':selected').text();
+});
 
 
 function LoadMemberName()
